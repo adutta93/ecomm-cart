708f118df8264b8c2abc3c6c23fae5cbcf767011
akash-dutta-au7 <dutta.akash1993official@gmail.com>
2021-11-01 14:44:05 +0530

    team channel list added

--- client/src/Components/ChannelContainer/index.jsx
+++ client/src/Components/ChannelContainer/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ChannelList, useChatContext } from "stream-chat-react";
 import Cookies from "universal-cookie";
 import ChannelSearch from "./ChannelSearch";
+import TeamChannelList from "./TeamChannelList";
 import HospitalIcon from "../../assets/hospital.png";
 import LogoutIcon from "../../assets/logout.png";
 
@@ -33,6 +34,11 @@ const ChannelContainer = () => {
       <div className="channel-list__list__wrapper">
         <CompanyHeader />
         <ChannelSearch />
+        <ChannelList
+          filters={{}}
+          channelRenderFilterFn={() => {}}
+          List={(listProps) => <TeamChannelList {...listProps} type="team" />}
+        />
       </div>
     </>
   );
